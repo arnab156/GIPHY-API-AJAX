7eeb6e081f9d045d5c61463e18a7c8e594735a53
arnab156 <38442084+arnab156@users.noreply.github.com>
2018-08-01 12:37:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 var topics = ["roger federer","rafael nadal", "serena williams", "venus williams", 'martina navratilova', "pete sampras", "sania mirza", "martina hingis","dominic thiem","andy murray"];
 console.log(topics);
 function renderButtons() {
-    // Deleting the movie buttons prior to adding new movie buttons
-    // (this is necessary otherwise we will have repeat buttons)
     $("#btnPanel").empty();
-    // Looping through the array of movies
     for (var i = 0; i < topics.length; i++) {
-        // Then dynamicaly generating buttons for each movie in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
         var a = $("<button>");
-        // Adding a class
         a.addClass("btn");
-        // Adding a data-attribute with a value of the movie at index i
         a.attr("data-name", topics[i]);
-        // Providing the button's text with a value of the movie at index i
         a.text(topics[i]);
-        // Adding the button to the HTML
         $("#btnPanel").append(a);
     }
 }
